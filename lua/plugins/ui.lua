2acdbcb40894f3ca4f2d2a77d32b877ea1829518
M Akbar Maulana <makbarm.id@gmail.com>
2024-03-16 00:11:32 +0700

    feat(plugins): configure bufferline behaviour
    - Only show tabpages
    - Disable close button

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,4 +20,20 @@ return {
       timeout = 1000,
     }
   },
+
+  -- Bufferline
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<Tab>",   "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false
+      }
+    }
+  }
 }
