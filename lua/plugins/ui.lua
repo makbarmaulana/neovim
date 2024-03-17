b2f09eaaf078acd0b43a5e0469c46291225187b6
M Akbar Maulana <makbarm.id@gmail.com>
2024-03-18 04:11:15 +0700

    feat(plugins): add floating filename at top panel with incline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,32 +8,67 @@ return {
           event = "notify",
           find = "No information available",
         },
-        opts = { skip = true }
+        opts = { skip = true },
       })
 
       opts.presets.lsp_doc_border = true
-    end
+    end,
   },
   {
     "rcarriga/nvim-notify",
     opts = {
       timeout = 1000,
-    }
+    },
   },
 
   -- Bufferline
   {
     "akinsho/bufferline.nvim",
     keys = {
-      { "<Tab>",   "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
       { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
     },
     opts = {
       options = {
         mode = "tabs",
         show_buffer_close_icons = false,
-        show_close_icon = false
-      }
-    }
-  }
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- Incline
+  {
+    "b0o/incline.nvim",
+    config = function()
+      require("incline").setup({
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+
+          if filename == "" then
+            filename = "[No Name]"
+          end
+
+          local helpers = require("incline.helpers")
+          local devicons = require("nvim-web-devicons")
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+
+          return {
+            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+            " ",
+            { filename, gui = modified and "bold,italic" or "bold" },
+            " ",
+            guibg = "#44406e",
+          }
+        end,
+      })
+    end,
+
+    event = "VeryLazy",
+  },
 }
